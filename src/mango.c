c110e45fd45a5095eb9f694f7b34bc2acd9bd135
DreamMaoMao <2523610504@qq.com>
2026-07-25 20:18:51 +0800

    opt: remove debug log

--- src/mango.c
+++ src/mango.c
@@ -4048,7 +4048,6 @@ void focusclient(Client *c, int32_t lift) {
 
 	/* Raise client in stacking order if requested */
 	if (c && lift) {
-		wlr_log(WLR_ERROR, "%s", client_get_title(c));
 		client_raise_group(c);
 	}
 
