db168929c455f37683dd9ab636c2f80b8616d8f8
Nikita Mitasov <me@ch4og.com>
2026-08-09 18:56:13 +0300

    fix(guix): remove temporary scenefx override

--- mangowm.scm
+++ mangowm.scm
@@ -61,7 +61,7 @@
                   pixman
                   xcb-util-wm
                   wlroots-0.20
-                  scenefx-0.5))
+                  scenefx))
     (native-inputs (list pkg-config wayland-protocols))
     (home-page "https://github.com/mangowm/mango")
     (synopsis "Wayland compositor based on wlroots and scenefx")
@@ -71,23 +71,6 @@ built on dwl — crafted for speed, flexibility, and a customizable desktop expe
     (license (list license:gpl3 ;mangowm itself, dwl
                    license:expat ;dwm, sway, wlroots
                    license:cc0)))) ;tinywl
-
-(define-public scenefx-0.5
-  (package
-    (inherit scenefx)
-    (name "scenefx")
-    (version "0.5")
-    (source (origin
-              (method git-fetch)
-              (uri (git-reference
-                     (url "https://github.com/wlrfx/scenefx")
-                     (commit version)))
-              (file-name (git-file-name name version))
-              (sha256
-               (base32
-                "0klxy73125lp9jab8qghh4v6di91l3y2rgan4m4lhv5flwdwnj5x"))))
-    (inputs (modify-inputs (package-inputs scenefx)
-              (replace "wlroots" wlroots-0.20)))))
 
 (define-deprecated-package mangowc
   mangowm-git)
